8db4b281e75de46e7d1fb00115f887af3bee9f5b
fx <fengxgoo@gmail.com>
2020-10-16 00:30:31 +0800

    parse head

--- peParse/peParse/peParse.cpp
+++ peParse/peParse/peParse.cpp
@@ -2,10 +2,140 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+void pause() {
+    char c;
+    std::cin >> c;
+}
+
+char* g_buf;
+
+char* loadFile(string fileName) {
+    FILE* file;
+    if (0 != fopen_s(&file, fileName.c_str(), "rb")) {
+        return 0;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long lSize = ftell(file);
+
+    char* buf = new char[lSize];
+    memset(buf, 0, lSize);
+
+    fseek(file, 0, SEEK_SET);
+    if (0 >= fread_s(buf, lSize, lSize, 1, file)) {
+        fclose(file);
+        return 0;
+    }
+
+    return buf;
+}
+
+#include<Windows.h>
+
+IMAGE_DOS_HEADER getDosHead(char* buf) {
+    IMAGE_DOS_HEADER head;
+    memcpy_s(&head, sizeof(head), buf, sizeof(head));
+    return head;
+}
+
+IMAGE_NT_HEADERS32 getNtHead32(char *buf) {
+    IMAGE_NT_HEADERS32 head;
+    memcpy_s(&head, sizeof(head), buf, sizeof(head));
+    return head;
+}
+
+IMAGE_NT_HEADERS64 getNtHead64(char* buf) {
+    IMAGE_NT_HEADERS64 head;
+    memcpy_s(&head, sizeof(head), buf, sizeof(head));
+    return head;
+}
+
+void getSectionHeader(char *bufTmp, IMAGE_SECTION_HEADER sectionHeaders[16], int n)
+{
+    memcpy_s(sectionHeaders, sizeof(sectionHeaders[0]) * 16, bufTmp, sizeof(sectionHeaders[0]) * 16);
+}
+
+
+int findSectionHeader(DWORD rva, IMAGE_SECTION_HEADER sectionHeaders[16], int n) {
+    for (int i = 0; i < n; i++) {
+        if (rva >= sectionHeaders[i].VirtualAddress && rva < (sectionHeaders[i].VirtualAddress + sectionHeaders[i].Misc.VirtualSize)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+void parseFile(unique_ptr<char[]> fileBuf) {
+    char* bufTmp = fileBuf.get();
+    IMAGE_DOS_HEADER dosHead = getDosHead(bufTmp);
+    
+    bufTmp += dosHead.e_lfanew;
+
+    IMAGE_NT_HEADERS32 ntHead32 = getNtHead32(bufTmp);
+    IMAGE_NT_HEADERS64 ntHead64 = getNtHead64(bufTmp);
+
+    int numberOfSections = ntHead32.FileHeader.NumberOfSections;
+    DWORD resourceRVA = 0;
+
+    switch (ntHead32.FileHeader.Machine) {
+    case IMAGE_FILE_MACHINE_I386:
+        printf("x86\n");
+        bufTmp += sizeof(ntHead32);
+        resourceRVA = ntHead32.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE].VirtualAddress;
+        break;
+    case IMAGE_FILE_MACHINE_AMD64:
+        printf("x64\n");
+        bufTmp += sizeof(ntHead64);
+        resourceRVA = ntHead64.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE].VirtualAddress;
+        break;
+    default:
+        printf("error\n");
+        return;
+    }
+
+    IMAGE_SECTION_HEADER sectionHeaders[16];
+    getSectionHeader(bufTmp, sectionHeaders, numberOfSections);
+
+    //解析resource
+    int resSectionIndex = findSectionHeader(resourceRVA, sectionHeaders, numberOfSections);
+    if (resSectionIndex < 0) {
+        return;
+    }
+
+
+}
+
+int main(int n, char **argv)
 {
     std::cout << "Hello World!\n";
+    string sIn;
+    if (n >= 2) {
+        sIn = argv[1];
+    }
+    else {
+       // std::cin >> sIn;
+        getline(cin, sIn, '\n');
+    }
+
+    std::cout << sIn << std::endl;
+
+    char* buf = loadFile(sIn);
+    if (buf == 0) {
+        cout << "load file failed" << endl;
+        return 0;
+    }
+
+    unique_ptr<char[]> fileBuf(buf);
+
+    parseFile(move(fileBuf));
+
+    pause();
+
+    return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
