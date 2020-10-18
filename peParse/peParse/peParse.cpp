4a2f989be42cca86272531d390ccb409d5b2128f
fx <fengxgoo@gmail.com>
2020-10-18 14:45:38 +0800

    parse import dll

--- peParse/peParse/peParse.cpp
+++ peParse/peParse/peParse.cpp
@@ -37,6 +37,11 @@ char* loadFile(string fileName) {
 IMAGE_DOS_HEADER getDosHead(char* buf) {
     IMAGE_DOS_HEADER head;
     memcpy_s(&head, sizeof(head), buf, sizeof(head));
+
+
+    cout << endl<< "[head]:" << endl;
+    cout << "head.e_magic:" << head.e_magic<<endl;
+    cout << "head.e_lfanew:" << head.e_lfanew << endl;
     return head;
 }
 
@@ -55,6 +60,12 @@ IMAGE_NT_HEADERS64 getNtHead64(char* buf) {
 void getSectionHeader(char *bufTmp, IMAGE_SECTION_HEADER sectionHeaders[16], int n)
 {
     memcpy_s(sectionHeaders, sizeof(sectionHeaders[0]) * 16, bufTmp, sizeof(sectionHeaders[0]) * 16);
+
+    cout << endl << "[section Header]:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "sectionHeaders["<<i<<"].Name:" << sectionHeaders[i].Name << "  |  ";
+        cout << "sectionHeaders["<<i<<"].VirtualAddress:"<<sectionHeaders[i].VirtualAddress << endl;
+    }
 }
 
 
@@ -134,22 +145,22 @@ void parseVersion(char* buf) {
     }
 }
 
-bool parseResourse(char*buf, DWORD resourceRVA, IMAGE_SECTION_HEADER& section, int level) {
+bool parseResourse(char* buf, DWORD resourceRVA, IMAGE_SECTION_HEADER& section, int level) {
     //1.rec directory
     IMAGE_RESOURCE_DIRECTORY direcotry;
     DWORD d = offsetPhyToVa(section);
-    char *bufPhy = buf + (resourceRVA + d);
+    char* bufPhy = buf + (resourceRVA + d);
     memcpy_s(&direcotry, sizeof(direcotry), bufPhy, sizeof(direcotry));
 
     char* bufPhySectionBase = buf + (section.VirtualAddress + d);
 
     //2.多个 directory entry
-    char * bufPhyEntyrBase = bufPhy + sizeof(direcotry);
+    char* bufPhyEntyrBase = bufPhy + sizeof(direcotry);
     int count = direcotry.NumberOfIdEntries + direcotry.NumberOfNamedEntries;
     //cout << "count: "<< count << endl;
     for (int i = 0; i < count; i++) {
         IMAGE_RESOURCE_DIRECTORY_ENTRY entry;
-        char *bufPhyEntry = bufPhyEntyrBase + i*sizeof(entry);
+        char* bufPhyEntry = bufPhyEntyrBase + i * sizeof(entry);
         memcpy_s(&entry, sizeof(entry), bufPhyEntry, sizeof(entry));
 
         //打印
@@ -161,7 +172,7 @@ bool parseResourse(char*buf, DWORD resourceRVA, IMAGE_SECTION_HEADER& section, i
         }
         else {
             //cout << "id: " << entry.Id << endl;
-            
+
         }
 
         if (1 == entry.DataIsDirectory) {
@@ -176,7 +187,7 @@ bool parseResourse(char*buf, DWORD resourceRVA, IMAGE_SECTION_HEADER& section, i
         else {
             IMAGE_RESOURCE_DATA_ENTRY dataEntry;
             memcpy_s(&dataEntry, sizeof(dataEntry), bufPhySectionBase + entry.OffsetToData, sizeof(dataEntry));
-            
+
 
             if (g_bVersion == true) {
                 parseVersion(buf + entry.OffsetToData);
@@ -186,14 +197,113 @@ bool parseResourse(char*buf, DWORD resourceRVA, IMAGE_SECTION_HEADER& section, i
     return true;
 }
 
-bool checkDebug(char* buf, DWORD resourceRVA, IMAGE_SECTION_HEADER &section) {
+bool checkDebug(char* bufPhyFile, DWORD resourceRVA, IMAGE_SECTION_HEADER& section) {
 
-    DWORD offset = offsetPhyToVa(section);
+    //DWORD offset = offsetPhyToVa(section);
 
-    parseResourse(buf,resourceRVA, section, 0);
+    parseResourse(bufPhyFile, resourceRVA, section, 0);
 
     return true;
 }
+
+
+bool checkImport(char* bufPhyFile, DWORD importSectionRVA, IMAGE_SECTION_HEADER& section) {
+    cout << endl;
+
+    DWORD offset = offsetPhyToVa(section);
+    char* bufPhyImportDescriptor = bufPhyFile + importSectionRVA + offset;
+
+    IMAGE_IMPORT_DESCRIPTOR desc;
+
+    while (true) {
+        
+        //每个dll文件，有一个IMAGE_IMPORT_DESCRIPTOR。 characteristics为0的时候结束
+        memcpy_s(&desc, sizeof(desc), bufPhyImportDescriptor, sizeof(desc));
+        if (desc.Characteristics == 0) {
+            break;
+        }
+
+        char * bufName = bufPhyFile + (desc.Name + offset);
+
+        cout << endl<< "[import DLL name: "<< bufName << "]" << endl;
+        //desc.OriginalFirstThunk 指向 导入表数组
+        IMAGE_THUNK_DATA thunk;
+        char* bufPhyThunk = bufPhyFile + (desc.OriginalFirstThunk + offset);
+
+        
+        //加载前, desc.OriginalFirstThunk 和 desc.FirstThunk 都指向 函数名表，
+        //加载后，desc.FirstThunk 指向 函数地址表
+
+        while (true) {
+            //每个 导入函数，有一个thunk。
+            memcpy_s(&thunk, sizeof(thunk), bufPhyThunk, sizeof(thunk));
+            
+            //IMAGE_SNAP_BY_ORDINAL判断是否按序号导入，IMAGE_ORDINAL用来获取导入序号。
+            if (thunk.u1.Ordinal == 0) {
+                break;
+            }
+            else if((thunk.u1.Ordinal & 0x80000000) != 0) {
+                thunk.u1.Ordinal & 0xffff;
+            }
+            else {
+                char* bufPhyName = bufPhyFile + (thunk.u1.AddressOfData + offset);
+                IMAGE_IMPORT_BY_NAME name;
+                //memcpy_s(&name, sizeof(name), bufPhyName, sizeof(name));
+
+                //char* buf = new char[name.Hint + 1];
+                //buf[name.Hint] = 0;
+                //memcpy_s(buf, name.Hint, bufPhyName+2, name.Hint);
+                cout << "import func: " << (bufPhyName + 2) << endl;
+                
+            }
+
+            bufPhyThunk += sizeof(thunk);
+        }
+        bufPhyImportDescriptor += sizeof(desc);
+    };// while (desc.Characteristics != 0);
+    
+    return true;
+}
+//_IMAGE_EXPORT_DIRECTORY; 
+
+/*
+#define IMAGE_DIRECTORY_ENTRY_EXPORT          0   // Export Directory
+#define IMAGE_DIRECTORY_ENTRY_IMPORT          1   // Import Directory
+#define IMAGE_DIRECTORY_ENTRY_RESOURCE        2   // Resource Directory
+#define IMAGE_DIRECTORY_ENTRY_EXCEPTION       3   // Exception Directory
+#define IMAGE_DIRECTORY_ENTRY_SECURITY        4   // Security Directory
+#define IMAGE_DIRECTORY_ENTRY_BASERELOC       5   // Base Relocation Table
+#define IMAGE_DIRECTORY_ENTRY_DEBUG           6   // Debug Directory
+//      IMAGE_DIRECTORY_ENTRY_COPYRIGHT       7   // (X86 usage)
+#define IMAGE_DIRECTORY_ENTRY_ARCHITECTURE    7   // Architecture Specific Data
+#define IMAGE_DIRECTORY_ENTRY_GLOBALPTR       8   // RVA of GP
+#define IMAGE_DIRECTORY_ENTRY_TLS             9   // TLS Directory
+#define IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG    10   // Load Configuration Directory
+#define IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT   11   // Bound Import Directory in headers
+#define IMAGE_DIRECTORY_ENTRY_IAT            12   // Import Address Table
+#define IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT   13   // Delay Load Import Descriptors
+#define IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR 14   // COM Runtime descriptor
+*/
+const char* IMAGE_DATA_ENTRY_NAME[16] = {
+    "export",
+    "import",
+    "resource",
+    "exception",
+    "security",
+    "basereloc",
+    "debug",
+    "copyright",
+    "architecture",
+    "globalptr",
+    "tls",
+    "load_config",
+    "boud_import",
+    "iat",
+    "delay_import",
+    "com_descriptor",
+};
+
+
 
 void parseFile(unique_ptr<char[]> fileBuf) {
     char* bufTmp = fileBuf.get();
@@ -217,8 +327,15 @@ void parseFile(unique_ptr<char[]> fileBuf) {
         resourceRVA = ntHead64.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_RESOURCE].VirtualAddress;
     }
 
+    cout << endl << "[IMAGE_DIRECTORY_ENTRY]" << endl;
+    for (int i = 0; i < 16; i++) {
+        cout << i << ": "<< IMAGE_DATA_ENTRY_NAME[i]<< ": " << ntHead32.OptionalHeader.DataDirectory[i].VirtualAddress << endl;
+    }
 
+    cout << "file_align: " << ntHead32.OptionalHeader.FileAlignment << endl;
+    cout << "section align: " << ntHead32.OptionalHeader.SectionAlignment << endl;
 
+    //节表头
     IMAGE_SECTION_HEADER sectionHeaders[16];
     getSectionHeader(bufTmp, sectionHeaders, numberOfSections);
 
@@ -229,18 +346,23 @@ void parseFile(unique_ptr<char[]> fileBuf) {
     }
 
     checkDebug(fileBuf.get(), resourceRVA, sectionHeaders[resSectionIndex] );
+
+    DWORD importSectionRVA = ntHead32.OptionalHeader.DataDirectory[1].VirtualAddress;
+    int importSectionIndex = findSectionHeader(importSectionRVA, sectionHeaders, numberOfSections);
+
+    checkImport(fileBuf.get(), importSectionRVA, sectionHeaders[importSectionIndex]);
 }
 
 int main(int n, char **argv)
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!"<<endl;
     string sIn;
     if (n >= 2) {
         sIn = argv[1];
     }
     else {
-       // std::cin >> sIn;
-        getline(cin, sIn, '\n');
+       sIn = "C:\\fx\\code\\my\\pe_parse\\pe_parse\\peParse\\Debug\\peParse1.exe";
+        //getline(cin, sIn, '\n');
     }
 
     std::cout << sIn << std::endl;
